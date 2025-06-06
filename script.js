5903a310582ca49e55fb5215de156fe10bf65d3b
Dakota-kz <145559684+Dakota-kz@users.noreply.github.com>
2025-06-06 22:59:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+
+function getGrid(size = 16) {
+    const totalSquare = size * size;
+    for (let i = 0; i < totalSquare; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "blue";
+    });
+
+    div.addEventListener("mouseleave", () => {
+        div.style.backgroundColor = "blue";
+    });
+    };
+};
+
+getGrid();
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+    container.innerHTML = '';
+    const size = Number(prompt());
+    getGrid(size);
+    const pixelSize = 960 / size;
+    const square = document.querySelectorAll('.square');
+    square.forEach(div => {
+        div.style.width = pixelSize + 'px';
+        div.style.height = pixelSize + 'px';
+    });
+});
